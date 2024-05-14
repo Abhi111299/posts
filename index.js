a8e2be092b11bcfd68be690e0ba0730826355a03
unknown <abhishekyadav9992183177@gmail.com>
2024-05-14 22:34:11 +0530

    Added the basis code for server startup and the mongodb Database connection

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import * as dotenv from 'dotenv'
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from "mongoose";
+
+import {CONNECTION_STRING} from './config/db.config.js';
+import errorHandler from './helper/error_handler.js';
+
+const app = express();
+
+app.use(cors());
+const corsOptions = {
+	origin: true,
+	credentials: true
+}
+app.options('*', cors(corsOptions)); // preflight OPTIONS; put before other routes
+
+app.use (bodyParser.json({limit: '50mb'})); // parse requests of content-type - application/json
+app.use (bodyParser.urlencoded({extended:true, limit: '50mb'})); // parse requests of content-type - application/x-www-form-urlencoded
+console.log(errorHandler);
+app.use(errorHandler);
+
+app.use(express.static('public'));// make the public foilders files available publicly
+
+dotenv.config(); // loading all the .env variables
+
+mongoose.set('strictQuery', false);
+app.listen(process.env.PORT, async () => {
+	const mongoDB = CONNECTION_STRING;
+	await mongoose.connect(mongoDB);
+	if (mongoose.connection.readyState){
+		console.log(`mongo db connected successfully => ${mongoDB}`);
+	}
+    
+	console.info(`app started listening on the port ${process.env.PORT}`);
+    console.info(`server is running at ${process.env.HOST}:${process.env.PORT}`);
+});
+
+export default app;
